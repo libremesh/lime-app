960c7973d9fff378818aca9f4cff64efe4eecad4
Marcos Gutierrez <gmarcos87@gmail.com>
2017-04-03 16:43:06 -0300

    Replace navbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,13 +2,23 @@ import { h, Component } from 'preact';
 import style from './style';
 
 import { Navs } from '../../routes';
+import { Drawer } from '../drawer';
 
 class Header extends Component {
+  state = { open: false };
+
+  toggle(){
+    this.setState({open:!this.state.open});
+  }
+
   render() {
     return (
       <header class={style.header}>
         <h1>LimeApp</h1>
-        <Navs />
+        <span onClick={this.toggle.bind(this)} class={style.menuIcon}>X</span>
+        <Drawer status={this.state.open} toggle={this.toggle.bind(this)}>
+          <Navs />
+        </Drawer>
       </header>
     );
   }
