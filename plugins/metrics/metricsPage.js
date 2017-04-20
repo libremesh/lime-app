56976a6b0a05bf6cbdfbd001b66d5d1b5858a1b8
Marcos Gutierrez <gmarcos87@gmail.com>
2017-04-20 10:39:42 -0300

    Spinner and Design changes

--- plugins/metrics/metricsPage.js
+++ plugins/metrics/metricsPage.js
@@ -2,11 +2,16 @@ import { h, Component } from 'preact';
 
 import style from './style';
 import I18n from 'i18n-js';
-
+window.i18n = I18n;
 import { bindActionCreators } from 'redux';
 import { connect } from 'preact-redux';
 
 import { getMetrics } from './metricsActions';
+
+import Loading from './components/loading';
+
+import colorScale from 'simple-color-scale';
+
 
 class Metrics extends Component {
   componentWillMount() {
@@ -14,22 +19,49 @@ class Metrics extends Component {
   }
   showButton(loading) {
     if (!loading) {
-      return (<button class="button green block" type="submit" translate='yes' onClick={this.props.getMetrics}>Get metrics</button>)
+      return (
+        <button class="button green block" type="submit" translate='yes' onClick={this.props.getMetrics}>
+          Get metrics
+        </button>
+      );
     }
-    return (<span translate="yes">Loading metrics...</span>) ;
+    return (
+      <div>
+      </div>
+    );
   }
+
+  showLoading(loading) {
+    if (!loading) {
+      return;
+    }
+    return (
+      <div style={{paddingTop:'50px'}}>
+        <Loading></Loading>
+        <span class="text-loading">{I18n.translateWithoutI18nline(this.props.metrics.status)}</span>
+      </div>
+    );
+  }
+
+  isGateway(hostname, gateway) {
+    return (hostname === gateway)? (
+      <span><img src=""/></span>
+    ) : (false);
+  };
   render() {
     return (
-      <div class="container" style={{paddingTop:'100px'}}>
+      <div class="container" style={{paddingTop:'50px'}}>
+        {this.showLoading(this.props.metrics.loading)}<br />
         <ul class="box">
           {this.props.metrics.metrics.map(station => (
-            <li><b>{station.hostname}</b><br/>
-                {station.bandwidth} Mbps<br/>
-                <span translate="yes">Package loss</span> {station.loss}%<br/>                
-                <div class="line" style={{outerWidth:(station.bandwidth*100/this.props.metrics.metrics[0].bandwidth).toString()+'%'}}></div>
+            <li class={(station.loading)? 'loading': 'load'}><b>{station.hostname}</b>
+                {this.isGateway(station.hostname, this.props.metrics.gateway)}
+                <br/>
+                {station.bandwidth} Mbps / <span translate="yes">Package loss</span> {station.loss}%<br/>                
+                <div class="line" style={{width:(station.bandwidth*100/this.props.metrics.metrics[0].bandwidth).toString()+'%',backgroundColor: colorScale.getColor(100-station.bandwidth*100/this.props.metrics.metrics[0].bandwidth)}}></div>
             </li>))}
         </ul>
-        {this.showButton(this.props.metrics.loading)}
+        {this.showButton(this.props.metrics.loading)}<br />
       </div>
     );
   }
