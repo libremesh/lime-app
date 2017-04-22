6119e2b4a724b0abb9b0bbc3908ea8ea7dbe07e5
Marcos Gutierrez <gmarcos87@gmail.com>
2017-04-22 02:33:51 -0300

    Enable tasks module

--- src/config.js
+++ src/config.js
@@ -2,11 +2,13 @@ import Meta   from '../plugins/meta';
 import Align  from '../plugins/align';
 import Locate from '../plugins/locate';
 import Metrics from '../plugins/metrics';
+import Tasks from '../plugins/tasks';
 
 // REGISTER PLUGINS
 export const plugins = [
   Meta,
   Align,
   Locate,
-  Metrics
+  Metrics,
+  Tasks
 ];
