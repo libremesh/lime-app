45d23f9f4d232df4b43f574545f158ce653b8096
Marcos Gutierrez <gmarcos87@gmail.com>
2017-05-22 13:13:23 -0300

    Added Metrics plugin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import Meta   from '@libremesh/limeapp-plugin-core';
 import Align  from '@libremesh/limeapp-plugin-align';
 import Locate from '../plugins/locate';
-import Metrics from '../plugins/metrics';
+import Metrics from '@libremesh/limeapp-plugin-metrics';
 import Tasks from '../plugins/tasks';
 import Rx from '@libremesh/limeapp-plugin-rx';
 
