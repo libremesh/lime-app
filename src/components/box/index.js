0c0a99f7f5b6156469b71eaff05e7668d5602af4
Marcos Gutierrez <gmarcos87@gmail.com>
2018-05-05 14:28:30 -0300

    style(style): Change app color

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -2,7 +2,7 @@ import { h } from 'preact';
 
 export const Box = ({ title, children }) => (
 	<div style={{ marginBottom: '10px' }}>
-		<div style={{ background: '#90d504',padding: '10px',color: '#fff' }}>
+		<div style={{ background: 'rgb(68, 146, 127)',padding: '10px',color: '#fff' }}>
 			<b>{title}</b>
 		</div>
 		<div style={{ border: '1px solid #ccc',padding: '10px' }}>
