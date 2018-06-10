78a8263e462332cc01463058e2f40ed930ccd01f
Marcos Gutierrez <gmarcos87@gmail.com>
2018-06-10 18:24:59 -0300

    fix(url): fix ground routing url in menu

--- plugins/lime-plugin-ground-routing/src/groundRoutingMenu.js
+++ plugins/lime-plugin-ground-routing/src/groundRoutingMenu.js
@@ -3,5 +3,5 @@ import { h } from 'preact';
 import I18n from 'i18n-js';
 
 export const Menu = () => (
-	<a href={'#/ground-routing'}>{I18n.t('groundRouting')}</a>
+	<a href={'#/groundrouting'}>{I18n.t('Ground Routing')}</a>
 );
