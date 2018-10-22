696fc98fa68455ee2b6645ad9017cb46b232378b
Marcos Gutierrez <gmarcos87@gmail.com>
2018-10-22 15:04:42 -0300

    fix(fbw): change fbw api

--- plugins/lime-plugin-fbw/src/api.js
+++ plugins/lime-plugin-fbw/src/api.js
@@ -1,8 +1,8 @@
-export const getStatus = (api, sid) => api.call(sid, 'firstbootwizard', 'status', {});
+export const getStatus = (api, sid) => api.call(sid, 'lime-fbw', 'status', {});
 
-export const searchNetworks = (api, sid, rescan) => api.call(sid, 'firstbootwizard', 'search_networks', { scan: rescan || false });
+export const searchNetworks = (api, sid, rescan) => api.call(sid, 'lime-fbw', 'search_networks', { scan: rescan || false });
 
-export const setNetwork = (api, sid, file) => api.call(sid, 'firstbootwizard', 'set_network', { file });
+export const setNetwork = (api, sid, file) => api.call(sid, 'lime-fbw', 'set_network', { file });
 
 // network -> { name: String }
-export const createNetwork = (api, sid, network ) => api.call(sid, 'firstbootwizard', 'create_network', network);
+export const createNetwork = (api, sid, network ) => api.call(sid, 'lime-fbw', 'create_network', network);
