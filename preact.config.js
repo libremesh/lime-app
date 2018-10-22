6896bb05867c7c341efe0bda56acd215e20cfee4
Marcos Gutierrez <gmarcos87@gmail.com>
2018-10-22 15:18:17 -0300

    build(webpath): select web path via .env configuration

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 /**
  * Function that mutates original webpack config.
  * Supports asynchronous changes when promise is returned.
@@ -8,5 +10,5 @@
  **/
 export default function (config, env, helpers) {
 	// Basepath of lime-app in the router: http://thisnode.info/app/
-	config.output.publicPath = '/app/';
+	config.output.publicPath = process.env.WEB_PATH || '/';
 }
