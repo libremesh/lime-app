e6c2d0fba7cea95be359b74dcee5f9d22425f869
Marcos Gutierrez <gmarcos87@gmail.com>
2019-02-25 19:24:26 -0300

    ci(lime-packages): update Makefile in lime-packages after deploy

---
+++ .ci/send_pull_request.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
+
+get_repo(){
+    git clone git@github.com:libremesh/lime-packages.git --branch master --depth 1 lime-packages
+    cd lime-packages
+}
+
+create_file() {
+    export HASH= sha256sum $TRAVIS_BUILD_DIR/lime-app-$TRAVIS_BRANCH.tar.gz
+    cat ../Makefile.temp | sed -e "s/\${i}/1/" \
+        | sed -e "s/\${version}/$HASH/" \
+        | sed -e "s/\${hash}/$TRAVIS_BRANCH/"  >> packages/lime-app/Makefile
+}
+
+commit_file() {
+  git checkout -b lime-app-$TRAVIS_BRANCH
+  git commit -am "LimeApp updated to $TRAVIS_BRANCH"
+}
+
+push_changes(){
+    git push --quiet lime-app-$TRAVIS_BRANCH
+}
+
+cd .ci
+setup_git
+get_repo
+create_file
+commit_file
+push_changes
