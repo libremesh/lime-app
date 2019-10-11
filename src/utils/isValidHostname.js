0237be932bc3111bd20380531b1a57dc4e9f5558
Marcos Gutierrez <gmarcos87@gmail.com>
2019-10-11 11:39:51 -0300

    fix(slugify): improves domain usage

--- src/utils/isValidHostname.js
+++ src/utils/isValidHostname.js
@@ -9,22 +9,24 @@ export const slugify = (string, end, domain) => {
 	const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
 	const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------';
 	const p = new RegExp(a.split('').join('|'), 'g');
-
+	
 	string = string
 		.toString()
 		.toLowerCase()
 		.replace(/\s+/g, '-') // Replace spaces with -
 		.replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
 		.replace(/&/g, '-and-') // Replace & with 'and'
-		.replace(/[^\w\-]+/g, '') // Remove all non-word characters
+		.replace(/[^-a-zA-Z0-9.]+/g, '') // Remove all non-word characters except - and .
 		.replace(/--+/g, '-') // Replace multiple - with single -
-		.replace(/^-+/, ''); // Trim - from start of text
+		.replace(/^-+/, '') // Trim - from start of text
+		.replace(/^\.+/, ''); // Trim . from start of text
 
 	if (!domain) {
 		string = string.replace('.', '-'); // Trim . from the text
 	}
 	if (end) {
 		string = string.replace(/-+$/, ''); // Trim - from end of text
+		string = string.replace(/\.+$/, ''); // Trim . from end of text
 	}
 
 	return string;
