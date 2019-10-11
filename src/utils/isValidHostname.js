48cdd4ed113af662b398b1973c88a1addabe2881
Marcos Gutierrez <gmarcos87@gmail.com>
2019-10-11 10:38:45 -0300

    feat(validation): hostname validation and slugify
    add helpers functions

---
+++ src/utils/isValidHostname.js
@@ -0,0 +1,31 @@
+export const isValidHostname = (text, length = false) => {
+	if (length) {
+		return /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(text) && text.length > 2;
+	}
+	return /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(text);
+};
+
+export const slugify = (string, end, domain) => {
+	const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+	const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+	const p = new RegExp(a.split('').join('|'), 'g');
+
+	string = string
+		.toString()
+		.toLowerCase()
+		.replace(/\s+/g, '-') // Replace spaces with -
+		.replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+		.replace(/&/g, '-and-') // Replace & with 'and'
+		.replace(/[^\w\-]+/g, '') // Remove all non-word characters
+		.replace(/--+/g, '-') // Replace multiple - with single -
+		.replace(/^-+/, ''); // Trim - from start of text
+
+	if (!domain) {
+		string = string.replace('.', '-'); // Trim . from the text
+	}
+	if (end) {
+		string = string.replace(/-+$/, ''); // Trim - from end of text
+	}
+
+	return string;
+};
