58f581da78a1479bc160c40cfeb5793fe138c34d
Marcos Gutierrez <gmarcos87@gmail.com>
2020-01-28 17:01:28 -0300

    fix(bug): 10.5.0.1 as home address

--- src/store.js
+++ src/store.js
@@ -23,6 +23,6 @@ const rootEpics =  combineEpics(...loadEpics(plugins));
 export const store = createStore({},rootEpics,rootReducers, new UhttpdService());
 
 // Init websocket
-export const initStore = () => store.dispatch({ type: 'meta/CONECTION_START', payload: 'http://10.5.0.1/ubus' });
+export const initStore = () => store.dispatch({ type: 'meta/CONECTION_START', payload: 'http://thisnode.info/ubus' });
 
 export default store;
