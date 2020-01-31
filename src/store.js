de56db7e6a7d66e769280631b5f41ac1ff554b07
Marcos Gutierrez <gmarcos87@gmail.com>
2020-01-31 12:48:07 -0300

    fix(navigation): fix ubus url
    Co-authored-by: germanferrero <ferrero.gf@gmail.com>
    
    Closes 226

--- src/store.js
+++ src/store.js
@@ -23,6 +23,9 @@ const rootEpics =  combineEpics(...loadEpics(plugins));
 export const store = createStore({},rootEpics,rootReducers, new UhttpdService());
 
 // Init websocket
-export const initStore = () => store.dispatch({ type: 'meta/CONECTION_START', payload: 'http://thisnode.info/ubus' });
+export const initStore = () => store.dispatch({
+	type: 'meta/CONECTION_START',
+	payload: window.location.href.split('/').slice(0, 3).join('/').concat('/ubus')
+});
 
 export default store;
