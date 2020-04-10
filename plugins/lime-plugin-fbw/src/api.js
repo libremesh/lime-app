16fdab5cce65f686e7c5dd32bca6790c61b773f4
German Ferrero <ferrero.gf@gmail.com>
2020-04-10 10:56:57 -0300

    style(fbw): linting

--- plugins/lime-plugin-fbw/src/api.js
+++ plugins/lime-plugin-fbw/src/api.js
@@ -1,8 +1,11 @@
-export const getStatus = (api, sid) => api.call(sid, 'lime-fbw', 'status', {});
+export const getStatus = (api, sid) =>
+    api.call(sid, 'lime-fbw', 'status', {});
 
-export const searchNetworks = (api, sid, rescan) => api.call(sid, 'lime-fbw', 'search_networks', { scan: rescan || false });
+export const searchNetworks = (api, sid, rescan) =>
+    api.call(sid, 'lime-fbw', 'search_networks', { scan: rescan || false });
 
-export const setNetwork = (api, sid, { file, hostname }) => api.call(sid, 'lime-fbw', 'set_network', { file, hostname });
+export const setNetwork = (api, sid, { file, hostname }) =>
+    api.call(sid, 'lime-fbw', 'set_network', { file, hostname });
 
-export const createNetwork = (api, sid, { network, hostname, password } ) =>
+export const createNetwork = (api, sid, { network, hostname, password }) =>
     api.call(sid, 'lime-fbw', 'create_network', { network, hostname, password });
