ccb253e709b1140dc362e1d10364a93f7ed07121
German Ferrero <ferrero.gf@gmail.com>
2021-01-25 15:50:18 -0300

    refactor(remotesupport): adapt to new api

--- plugins/lime-plugin-remotesupport/src/remoteSupportApi.js
+++ plugins/lime-plugin-remotesupport/src/remoteSupportApi.js
@@ -1,14 +1,14 @@
 import api from 'utils/uhttpd.service';
 
 export function getSession() {
-	return api.call("lime-remotesupport", "get_session", {}).toPromise()
-		.then(result => result.session !== 'not initialized' ? result.session : null)
+	return api.call("tmate", "get_session", {}).toPromise()
+		.then(result => result.session !== 'no session' ? result.session : null)
 }
 
 export function openSession() {
-	return api.call("lime-remotesupport", "open_session", {}).toPromise()
+	return api.call("tmate", "open_session", {}).toPromise()
 }
 
 export function closeSession() {
-	return api.call("lime-remotesupport", "close_session", {}).toPromise()
+	return api.call("tmate", "close_session", {}).toPromise()
 }
