274a1eb7aaf70d358ea7a804fb22925c693b97c5
German Ferrero <ferrero.gf@gmail.com>
2021-04-08 20:21:31 -0300

    chore(dev-tools): add test, stories and page contents to skeleton

--- devTools/plugins.js
+++ devTools/plugins.js
@@ -29,15 +29,108 @@ export default Menu;
 const pageContent = (name) => {
     const componentName = name.charAt(0).toUpperCase() + name.slice(1);
     return (
-        `import { h } from 'preact';
+        `// ${componentName} will be rendered when navigating to this plugin
+import { h } from 'preact';
+import style from './${name}Style.less';
+import { useSomething } from './${name}Queries';
 
 const ${componentName} = () => {
-    return;
+    const { data: something } = useSomething();
+    return (
+        <div>{ something }</div>
+    );
 }
 
 export default ${componentName};
 `)
 }
+
+const queriesContent = (name) =>
+    `// Here you define queries and mutations which connects the api with your components
+import { useQuery, useMutation } from 'react-query';
+import { getSomething } from './${name}Api';
+
+export const useSomething = () => useQuery(['package', 'command'], getSomething);
+`
+
+const apiContent = () =>
+    `// Here you define the api calls to the ubus uhttpd enpoints your plugin needs
+import api from 'utils/uhttpd.service';
+
+export const getSomething = () => api.call('package', 'command', {})
+`
+
+const apiSpecContent = (name) =>
+    `// Here you define unit tests for the api endpoints of this plugin
+import { getSomething } from './${name}Api'
+import api from 'utils/uhttpd.service';
+jest.mock('utils/uhttpd.service')
+
+beforeEach(() => {
+    api.call.mockImplementation(() => of({status: 'ok'}))
+})
+
+describe('getSomething', () => {
+    it('test some behaviour', async () => {
+        expect(true).toBeTrue();
+    });
+    it('test some other behaviour', async () => {
+        expect(true).toBeTrue();
+    });
+});
+`
+
+const specContent = (name) => {
+    const componentName = name.charAt(0).toUpperCase() + name.slice(1);
+    return (
+        `// Here you define tests that closely resemble how your component is used
+// Using the testing-library: https://testing-library.com
+
+import { h } from 'preact';
+import { screen, cleanup, act } from '@testing-library/preact';
+import '@testing-library/jest-dom';
+import { render } from 'utils/test_utils';
+import queryCache from 'utils/queryCache';
+
+import ${componentName} from './src/${name}Page';
+import { getSomething } from './src/${name}Api';
+
+jest.mock('./src/${name}Api');
+
+describe('${name}', () => {
+    beforeEach(() => {
+        getSomething.mockImplementation(async () => "something");
+    });
+
+    afterEach(() => {
+        cleanup();
+        act(() => queryCache.clear());
+    });
+
+    it('test that something is shown', async() => {
+        render(<${componentName} />);
+        expect(await screen.findByText('something')).toBeInTheDocument();
+    })
+});
+`)
+}
+
+const storiesContent = (name, menuName) => {
+    const componentName = name.charAt(0).toUpperCase() + name.slice(1);
+    return (
+        `//Here you define the StoryBook stories for this plugin
+
+import ${componentName} from './src/${name}Page';
+
+export default {
+    title: 'Containers/${menuName || name}'
+}
+
+export const myStory = () => <${componentName} />
+`);
+};
+
+const styleContent = () => "// Here you define the css for this plugin";
 
 const argv = yargs(hideBin(process.argv))
     .command('$0 create <name>', 'create an skeleton for a new plugin',
@@ -65,16 +158,15 @@ function create(argv) {
     baseDir = path.join(__dirname, '..', 'plugins', pluginName);
     fs.mkdirSync(baseDir);
     fs.mkdirSync(path.join(baseDir, 'src'))
-    fs.writeFileSync(path.join(baseDir, 'src', `${name}Page.js`),
-        pageContent(name));
-    fs.writeFileSync(path.join(baseDir, 'src', `${name}Queries.js`), '');
-    fs.writeFileSync(path.join(baseDir, 'src', `${name}Api.js`), '');
-    fs.writeFileSync(path.join(baseDir, 'src', `${name}Api.spec.js`), '')
-    fs.writeFileSync(path.join(baseDir, 'src', `${name}Menu.js`),
-        menuContent(name, argv.menuName)
-    );
-    fs.writeFileSync(path.join(baseDir, 'src', `${name}Style.less`), '');
-    fs.writeFileSync(path.join(baseDir, 'index.js'),
-        indexContent(name, argv.protected));
-    fs.writeFileSync(path.join(baseDir, `${name}.spec.js`), '');
+    fs.writeFileSync(path.join(baseDir, 'src', `${name}Page.js`), pageContent(name));
+    fs.writeFileSync(path.join(baseDir, 'src', `${name}Queries.js`), queriesContent(name));
+    fs.writeFileSync(path.join(baseDir, 'src', `${name}Api.js`), apiContent());
+    fs.writeFileSync(path.join(baseDir, 'src', `${name}Api.spec.js`), apiSpecContent(name));
+    fs.writeFileSync(path.join(baseDir, 'src', `${name}Menu.js`), menuContent(name, argv.menuName));
+    fs.writeFileSync(path.join(baseDir, 'src', `${name}Style.less`), styleContent());
+    fs.writeFileSync(path.join(baseDir, 'index.js'), indexContent(name, argv.protected));
+    fs.writeFileSync(path.join(baseDir, `${name}.spec.js`), specContent(name));
+    fs.writeFileSync(path.join(baseDir, `${name}.stories.js`), storiesContent(name));
+    const configPath = path.join(__dirname, '..', 'src', 'config.js');
+    console.log(`You are done. Remember to add ${name} to the list in ${configPath}`);
 }
