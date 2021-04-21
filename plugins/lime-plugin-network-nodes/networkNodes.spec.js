7f4d00e447b6e5331d3eb5b4a62fd6f80f42b5a6
German Ferrero <ferrero.gf@gmail.com>
2021-04-21 18:32:52 -0300

    improvement(network-nodes): filter gones and sort

--- plugins/lime-plugin-network-nodes/networkNodes.spec.js
+++ plugins/lime-plugin-network-nodes/networkNodes.spec.js
@@ -19,13 +19,22 @@ describe('networkNodes', () => {
                 ipv4: '10.5.0.16',
                 ipv6: 'fd0d:fe46:8ce8::8bbf:7500',
                 board: 'LibreRouter v1',
-                fw_version: 'LibreRouterOS 1.4'
+                fw_version: 'LibreRouterOS 1.4',
+                status: 'recently_connected'
             },
             "ql-nelson": {
                 ipv4: '10.5.0.17',
                 ipv6: 'fd0d:fe46:8ce8::8bbf:75bf',
                 board: 'LibreRouter v1',
-                fw_version: 'LibreRouterOS 1.4'
+                fw_version: 'LibreRouterOS 1.4',
+                status: 'disconnected'
+            },
+            "ql-gone-node": {
+                ipv4: '10.5.0.18',
+                ipv6: 'fd0d:fe46:8ce8::8bbf:75be',
+                board: 'LibreRouter v1',
+                fw_version: 'LibreRouterOS 1.4',
+                status: 'gone'
             }
         }));
     });
@@ -35,7 +44,7 @@ describe('networkNodes', () => {
         act(() => queryCache.clear());
     });
 
-    it('test that nodes are shown', async () => {
+    it('test that nodes recently_connected and connected nodes are shown', async () => {
         render(<NetworkNodes />);
         expect(await screen.findByText('ql-nelson')).toBeInTheDocument();
         expect(await screen.findByText('ql-berta')).toBeInTheDocument();
@@ -49,6 +58,12 @@ describe('networkNodes', () => {
         expect(await screen.findByText('IPv6: fd0d:fe46:8ce8::8bbf:75bf')).toBeInTheDocument();
         expect(await screen.findByText('Device: LibreRouter v1')).toBeInTheDocument();
         expect(await screen.findByText('Firmware: LibreRouterOS 1.4')).toBeInTheDocument();
+    });
+
+    it('test that gone nodes are not shown', async () => {
+        render(<NetworkNodes />);
+        await screen.findByText('ql-nelson');
+        expect(screen.queryByText('ql-gone-node')).toBeNull();
     })
 
 });
