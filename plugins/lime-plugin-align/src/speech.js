811a3ecf9bc61531b0be628b6512415a1ff19e07
Tomas Assenza <tomyassenza@gmail.com>
2021-07-11 05:32:03 -0300

    fixed(getVoices): used an if to check if the user has compability with speechsyntesis

--- plugins/lime-plugin-align/src/speech.js
+++ plugins/lime-plugin-align/src/speech.js
@@ -1,11 +1,13 @@
-let synth = window.speechSynthesis;
-let voices = synth.getVoices();
+const synth = window.speechSynthesis;
 
 export const speech = (text, lang) => {
-	let utterThis = new SpeechSynthesisUtterance(text);
-	utterThis.pitch = 0.9;
-	utterThis.rate = 1.2;
-	utterThis.voice = voices.filter(x => x.lang === lang)[0];
-	synth.cancel();
-	synth.speak(utterThis);
+	if(synth != "undefined") {
+		let voices = synth.getVoices();
+		let utterThis = new SpeechSynthesisUtterance(text);
+		utterThis.pitch = 0.9;
+		utterThis.rate = 1.2;
+		utterThis.voice = voices.filter(x => x.lang === lang)[0];
+		synth.cancel();
+		synth.speak(utterThis);
+	}
 };
