3e8d3a71b5739446c810d3083484b5f38464ea17
German Ferrero <ferrero.gf@gmail.com>
2021-07-16 18:03:46 -0300

    chore(Loading): add aria-label

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -3,7 +3,7 @@ import { h } from 'preact';
 import style from './style.less';
 
 export const Loading = () => (
-	<div className={style.spinner}>
+	<div className={style.spinner} aria-label="loading">
 		<div className={style.bounce1} />
 		<div className={style.bounce2} />
 		<div className={style.bounce3} />
