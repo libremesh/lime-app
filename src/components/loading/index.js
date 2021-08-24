69ec0b424590213ba8b11305f429beaffb8a0a2d
German Ferrero <ferrero.gf@gmail.com>
2021-08-24 17:32:21 -0300

    improvement(loading): let choose the color

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -2,12 +2,15 @@ import { h } from 'preact';
 
 import style from './style.less';
 
-export const Loading = () => (
-	<div className={style.spinner} aria-label="loading">
-		<div className={style.bounce1} />
-		<div className={style.bounce2} />
-		<div className={style.bounce3} />
-	</div>
-);
+export const Loading = ({ color }) => {
+	const styleOverride = color ? { 'backgroundColor': color } : '';
+	return (
+		<div className={style.spinner} aria-label="loading">
+			<div className={style.bounce1} style={styleOverride} />
+			<div className={style.bounce2} style={styleOverride} />
+			<div className={style.bounce3} style={styleOverride} />
+		</div>
+	)
+};
 
 export default Loading;
