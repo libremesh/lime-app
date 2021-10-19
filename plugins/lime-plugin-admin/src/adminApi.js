8c22f42e91f1cd17b995292c928637c8a0365535
German Ferrero <ferrero.gf@gmail.com>
2021-10-19 17:22:28 -0300

    chore(admin): remove old admin plugin

--- plugins/lime-plugin-admin/src/adminApi.js
+++
@@ -1,5 +0,0 @@
-export const changeHostname = (api, hostname) =>
-	api.call('lime-utils-admin', 'set_hostname', { hostname });
-
-export const getIpV4 = (api) =>
-	api.call('lime-utils', 'get_node_status', {})
--- plugins/lime-plugin-admin/src/adminConstants.js
+++
@@ -1,4 +0,0 @@
-export const SET_HOSTNAME = 'admin/SET_HOSTNAME';
-export const SET_HOSTNAME_SUCCESS = 'admin/SET_HOSTNAME_SUCCESS';
-export const SET_HOSTNAME_ERROR = 'admin/SET_HOSTNAME_ERROR';
-export const GET_IP_SUCCESS = 'admin/GET_IP_SUCCESS';
