bd268c0c82eff438105fb99a20e93a5e98ba876f
luandro <luandro@gmail.com>
2021-11-04 19:32:08 -0300

    chore(pirania): changes base64 icons to unicode

--- plugins/lime-plugin-pirania/src/components/goBack.js
+++ plugins/lime-plugin-pirania/src/components/goBack.js
@@ -2,12 +2,9 @@ import { h } from "preact";
 import { route } from "preact-router";
 
 const GoBack = ({ url }) => (
-	<img
-		style={{ cursor: "pointer" }}
+	<span
+		style={{ cursor: "pointer", fontSize: 50 }}
 		onClick={() => route(url)}
-		src={
-			"data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTE2IDdIMy44M0w5LjQyIDEuNDFMOCAwTDAgOEw4IDE2TDkuNDEgMTQuNTlMMy44MyA5SDE2VjdaIiBmaWxsPSJibGFjayIgZmlsbC1vcGFjaXR5PSIwLjU0Ii8+Cjwvc3ZnPgo="
-		}
-	/>
+	>🠔</span>
 );
 export default GoBack;
