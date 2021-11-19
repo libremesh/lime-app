1f97d076a4276566e5c30661e23ec47bc93d64a3
German Ferrero <ferrero.gf@gmail.com>
2021-11-19 14:11:57 -0300

    improvement(pirania): add img compression

--- plugins/lime-plugin-pirania/src/piraniaApi.js
+++ plugins/lime-plugin-pirania/src/piraniaApi.js
@@ -1,5 +1,5 @@
-// Here you define the api calls to the ubus uhttpd enpoints your plugin needs
 import api from 'utils/uhttpd.service';
+import Compressor from 'compressorjs';
 
 export const getPortalConfig = () =>
     api.call('pirania', 'get_portal_config', {});
@@ -13,12 +13,18 @@ export const getPortalContent = () =>
 export const setPortalContent = (content) =>
     api.call('pirania-app', 'write_content', content);
 
-export const createCompression = (logoFile) =>
+export const createCompression = (file) =>
     new Promise(res => {
-        const reader = new FileReader();
-        reader.onloadend = function () {
-            res(reader.result);
-        }
-        reader.readAsDataURL(logoFile);
+        new Compressor(file, {
+            quality: 0.6,
+            maxHeight: 150,
+            maxWidth: 150,
+            success: (result) =>  {
+                const reader = new FileReader();
+                reader.onloadend = function () {
+                    res(reader.result);
+                }
+                reader.readAsDataURL(result);
+            }
+        });
     });
-
