18b6a8421b57699125b57f9c93ceececa41cdfd5
selankon <selankon@selankon.xyz>
2022-03-01 15:26:56 +0100

    fix(fbw): unused parameter
    See https://github.com/libremesh/lime-app/pull/334/files/e5f27d0f55a2bb9b196089f17b4d9c7e6df77862#r809291076

--- plugins/lime-plugin-fbw/src/FbwApi.js
+++ plugins/lime-plugin-fbw/src/FbwApi.js
@@ -1,11 +1,10 @@
 import api from 'utils/uhttpd.service';
-// import { from } from 'rxjs';
 
 export const searchNetworks = (rescan) =>
 	api.call('lime-fbw', 'search_networks', { scan: rescan || false });
 
-export const setNetwork = ({ file, hostname, network }) =>
-	api.call('lime-fbw', 'set_network', { file, hostname, network });
+export const setNetwork = ({ file, hostname }) =>
+	api.call('lime-fbw', 'set_network', { file, hostname });
 
 export const createNetwork = ({ network, hostname, adminPassword }) =>
 	api.call('lime-fbw', 'create_network', { network, hostname, adminPassword });
