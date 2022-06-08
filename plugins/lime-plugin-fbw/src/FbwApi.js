73ae6a6ab7df966866ae14480eee52391ca13f69
selankon <selankon@selankon.xyz>
2022-06-08 09:43:38 +0200

    refactor(fbw): delete deprecated getFbwStatus

--- plugins/lime-plugin-fbw/src/FbwApi.js
+++ plugins/lime-plugin-fbw/src/FbwApi.js
@@ -9,9 +9,5 @@ export const setNetwork = ({ file, hostname }) =>
 export const createNetwork = ({ network, hostname, adminPassword }) =>
 	api.call('lime-fbw', 'create_network', { network, hostname, adminPassword });
 
-export const getFbwStatus = () =>
-	api.call('lime-fbw', 'status', {})
-		.catch(() => ({ lock: false }));
-
 export const dismissFbw = () =>
 	api.call('lime-fbw', 'dismiss', {});
--- plugins/lime-plugin-fbw/src/FbwQueries.js
+++ plugins/lime-plugin-fbw/src/FbwQueries.js
@@ -1,18 +1,11 @@
 import { useQuery, useMutation } from 'react-query';
 import queryCache from 'utils/queryCache';
-import { dismissFbw, getFbwStatus, createNetwork, searchNetworks, setNetwork }
+import { dismissFbw, createNetwork, searchNetworks, setNetwork }
 	from './FbwApi';
 
 export function useDismissFbw() {
 	return useMutation(dismissFbw, {
 		onSuccess: () => queryCache.setQueryData(['lime-fbw', 'status'], {lock: false})
-	});
-}
-
-export function useFbwStatus() {
-	return useQuery(['lime-fbw', 'status'], getFbwStatus, {
-		initialData: {lock: false},
-		initialStale: true
 	});
 }
 
