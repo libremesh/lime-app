06cc947508727ab199cef7f762a45221b8cca9e8
Luandro <luandro@digital-democracy.org>
2022-10-28 18:52:59 -0300

    feat(ux) add icons to menu

--- plugins/lime-plugin-network-admin/src/netAdminMenu.js
+++ plugins/lime-plugin-network-admin/src/netAdminMenu.js
@@ -3,5 +3,8 @@
 import { Trans } from '@lingui/macro';
 
 export const NetAdminMenu = () => (
-	<a href='#/netadmin'><Trans>Network Configuration</Trans></a>
+	<span>
+		<svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M4.5 6.5v-3a3 3 0 016 0v3m-8 0h10a1 1 0 011 1v6a1 1 0 01-1 1h-10a1 1 0 01-1-1v-6a1 1 0 011-1z" stroke="currentColor" /></svg>
+		<a href='#/netadmin'><Trans>Shared Password</Trans></a>
+	</span>
 );
--- plugins/lime-plugin-rx/src/rxMenu.js
+++ plugins/lime-plugin-rx/src/rxMenu.js
@@ -3,5 +3,8 @@
 import { Trans } from '@lingui/macro';
 
 export const RxMenu = () => (
-	<a href={'#/rx'}><Trans>Status</Trans></a>
+	<span>
+		<svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M5.5.5h-4a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1v-4a1 1 0 00-1-1zm8 0h-4a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1v-4a1 1 0 00-1-1zm0 8h-4a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1v-4a1 1 0 00-1-1zm-8 0h-4a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1v-4a1 1 0 00-1-1z" stroke="currentColor" /></svg>
+		<a href={'#/rx'}><Trans>Status</Trans></a>
+	</span>
 );
