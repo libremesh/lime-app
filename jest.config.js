714b787a2f6ed40cc6cb2ef263e1b591db2cc041
fnbellomo <fnbellomo@gmail.com>
2022-10-27 23:56:43 -0300

    chore(testing) fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,23 @@
+const preactPreset = require("jest-preset-preact");
+
 /** @returns {Promise<import('jest').Config>} */
 module.exports = {
     preset: "jest-preset-preact",
+    testMatch: [
+        "<rootDir>/plugins/**/__tests__/**/*.{mjs,js,jsx,ts,tsx}",
+        "<rootDir>/{plugins, src,test,tests}/**/*.{spec,test}.{mjs,js,jsx,ts,tsx}",
+    ],
+    setupFilesAfterEnv: ["jest-extended/all"],
+    setupFiles: ["core-js", "jest-localstorage-mock"],
     moduleNameMapper: {
+        ...preactPreset.moduleNameMapper,
         "^components/(.*)$": "<rootDir>/src/components/$1",
         "^utils/(.*)$": "<rootDir>/src/utils/$1",
         "^containers/(.*)$": "<rootDir>/src/containers/$1",
         "^plugins/(.*)$": "<rootDir>/plugins/$1",
+        "^i18n/(.*)$": "<rootDir>/i18n/$1",
     },
     transform: {
-        "^.+\\.[tj]sx?$": ["babel-jest"],
+        "^.+\\.[tj]sx?$": "babel-jest",
     },
-    clearMocks: true,
-    resetMocks: true,
-    setupFiles: ["jest-localstorage-mock"],
 };
