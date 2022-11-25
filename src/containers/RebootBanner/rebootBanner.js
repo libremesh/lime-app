0c395918a688c5fd307a6c933ee8d20d8c99dab2
selankon <selankon@selankon.xyz>
2022-11-25 09:05:22 +0100

    fix(tests): fix Matcher undefined
    - Importing `Match` instead of `{ Match }`
    - And using `"` instead of `'` on a `data-testid`
    
    Surprisingly seems to fix the tests...

--- src/containers/RebootBanner/rebootBanner.js
+++ src/containers/RebootBanner/rebootBanner.js
@@ -1,16 +1,22 @@
-
+import { Trans } from "@lingui/macro";
 import { route } from "preact-router";
-import { Match } from 'preact-router/match';
-import { Trans } from '@lingui/macro';
+import Match from "preact-router/match";
 
 export const RebootBanner = () => (
     <Match>
-        {({ path }) => (path !== '/reboot') &&
-            <div data-testid='changes-need-reboot'
-                class="subheader-notification" style={{ backgroundColor: "#f7a336" }}>
-                <Trans>A reboot is needed to apply changes</Trans>
-                <button onClick={() => route('/reboot')}><Trans>Reboot</Trans></button>
-            </div>
+        {({ path }) =>
+            path !== "/reboot" && (
+                <div
+                    data-testid="changes-need-reboot"
+                    class="subheader-notification"
+                    style={{ backgroundColor: "#f7a336" }}
+                >
+                    <Trans>A reboot is needed to apply changes</Trans>
+                    <button onClick={() => route("/reboot")}>
+                        <Trans>Reboot</Trans>
+                    </button>
+                </div>
+            )
         }
     </Match>
 );
