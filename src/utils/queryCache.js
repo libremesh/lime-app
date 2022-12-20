008607b5ca193e08905e00fbd6ecfb6acfb68033
fnbellomo <fnbellomo@gmail.com>
2022-12-20 00:27:04 -0300

    fix(react-query): dont log error on test
    To avoid break the ci

--- src/utils/queryCache.js
+++ src/utils/queryCache.js
@@ -1,13 +1,19 @@
-import { QueryClient } from '@tanstack/react-query'
+import { QueryClient } from "@tanstack/react-query";
 
 const queryCache = new QueryClient({
-	defaultConfig: {
-		queries: {
-			staleTime: Infinity,
-			refetchOnMount: false,
-			retry: 0
-		}
-	}
+    defaultConfig: {
+        queries: {
+            staleTime: Infinity,
+            refetchOnMount: false,
+            retry: 0,
+        },
+    },
+    logger: {
+        log: console.log,
+        warn: console.warn,
+        // ✅ no more errors on the console for tests
+        error: process.env.NODE_ENV === "test" ? () => {} : console.error,
+    },
 });
 
 export default queryCache;
