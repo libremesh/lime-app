500212e40c7c5a52c729c163f5842b5c235e90d8
selankon <selankon@selankon.xyz>
2023-02-10 16:38:17 +0100

    chore(tailwindcss): Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: "jit",
+    content: [],
+    purge: [
+        "./src/**/*.{js,jsx,ts,tsx,vue}",
+        "./plugins/**/*.{js,jsx,ts,tsx,vue}",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
