daa77039461e5cf419a1d57456e406449778d842
selankon <selankon@selankon.xyz>
2023-02-20 09:18:53 +0100

    chore(tailwind): add primary color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#38927f",
+                    light: "#6BC3AE",
+                    dark: "#006453",
+                },
+            },
+        },
     },
     plugins: [],
 };
