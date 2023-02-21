75d9c837b0568b26599e5016c506f73ebed92dc0
selankon <selankon@selankon.xyz>
2023-02-21 09:28:35 +0100

    chore(rxPage): fix section title color

--- plugins/lime-plugin-rx/src/components/components.tsx
+++ plugins/lime-plugin-rx/src/components/components.tsx
@@ -10,16 +10,16 @@ export const SectionTitle: FunctionalComponent<SectionTitleProps> = ({
     icon,
 }) => {
     return (
-        <div className="flex">
-            {icon}
-            <h1 className="text-4xl font-bold text-center mb-8">{children}</h1>
+        <div className="flex ">
+            <span className={"text-primary-dark fill-current"}>{icon}</span>
+            <h1 className="text-4xl font-bold text-left mb-8">{children}</h1>
         </div>
     );
 };
 
 export const Section = ({ ...props }) => {
     return (
-        <div className="flex-1 bg-gray-200">
+        <div className="flex-1">
             <div className="container mx-auto px-4 py-8">{props.children}</div>
         </div>
     );
