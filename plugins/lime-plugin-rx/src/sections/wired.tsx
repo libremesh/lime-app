08d575a9b6eb346bef9e5f38a9246b506c6f8e57
selankon <selankon@selankon.xyz>
2023-02-21 14:48:19 +0100

    chore(rxPage): fix paddings and sizes

--- plugins/lime-plugin-rx/src/sections/wired.tsx
+++ plugins/lime-plugin-rx/src/sections/wired.tsx
@@ -1,6 +1,7 @@
 import { Trans } from "@lingui/macro";
 
 import {
+    IconsClassName,
     Section,
     SectionTitle,
 } from "plugins/lime-plugin-rx/src/components/components";
@@ -9,7 +10,7 @@ import { PortsIcon } from "plugins/lime-plugin-rx/src/icons/portsIcon";
 export const Wired = () => {
     return (
         <Section>
-            <SectionTitle icon={<PortsIcon />}>
+            <SectionTitle icon={<PortsIcon className={IconsClassName} />}>
                 <Trans>Wired connections</Trans>
             </SectionTitle>
         </Section>
