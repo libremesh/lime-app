877e4d29bb90d48f2a115b7850ac716c6264b6c8
selankon <selankon@selankon.xyz>
2023-02-21 09:28:57 +0100

    chore(rxPage): create path component

--- plugins/lime-plugin-rx/src/sections/internetPath.tsx
+++ plugins/lime-plugin-rx/src/sections/internetPath.tsx
@@ -6,11 +6,20 @@ import {
 } from "plugins/lime-plugin-rx/src/components/components";
 import { PathIcon } from "plugins/lime-plugin-rx/src/icons/pathIcon";
 
+import LineChart from "../components/path";
+
+const data = [
+    { ip: "10.219.105.28", hostname: "nodo11s" },
+    { ip: "10.1.61.129", hostname: "" },
+    { ip: "10.75.0.1", hostname: "" },
+];
+
 export const InternetPath = () => {
     return (
         <Section>
-            <SectionTitle icon={<PathIcon />}>
+            <SectionTitle icon={<PathIcon className={"h-16 w-16"}/>}>
                 <Trans>Path to Internet</Trans>
+                <LineChart data={data} />
             </SectionTitle>
         </Section>
     );
