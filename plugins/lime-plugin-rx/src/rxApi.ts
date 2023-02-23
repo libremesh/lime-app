8b9a11dde7e4ed5cf884a32c2b9b88edd827ad3c
selankon <selankon@selankon.xyz>
2023-02-23 14:34:26 +0100

    chore(rxPage): migrate to TS

--- plugins/lime-plugin-rx/src/rxApi.ts
+++ plugins/lime-plugin-rx/src/rxApi.ts
@@ -1,12 +1,69 @@
 import api from "utils/uhttpd.service";
 
-export const getNodeStatus = () => {
+interface DeviceStatus {
+    version: string;
+    address: string;
+}
+
+interface SwitchStatus {
+    device: string;
+    num: number;
+    role: string;
+    link: string;
+}
+
+interface MostActive {
+    rx_short_gi: boolean;
+    station_mac: string;
+    tx_bytes: number;
+    rx_vht: boolean;
+    rx_mhz: number;
+    rx_40mhz: boolean;
+    tx_packets: number;
+    tx_mhz: number;
+    rx_packets: number;
+    rx_ht: boolean;
+    tx_mcs: number;
+    noise: number;
+    rx_mcs: number;
+    chains: number[];
+    rx_bytes: number;
+    tx_ht: boolean;
+    iface: string;
+    tx_rate: number;
+    inactive: number;
+    tx_short_gi: boolean;
+    tx_40mhz: boolean;
+    expected_throughput: number;
+    tx_vht: boolean;
+    rx_rate: number;
+    signal: number;
+}
+
+export interface StatusResponse {
+    ips: DeviceStatus[];
+    hostname: string;
+    switch_status: SwitchStatus[];
+    status: string;
+    uptime: string;
+    most_active: MostActive;
+}
+
+export interface IGetInternetStatus {
+    DNS: { working: boolean };
+    IPv6: { working: boolean };
+    IPv4: { working: boolean };
+}
+
+export const getNodeStatus = async (): Promise<StatusResponse> => {
     return mock_node_status;
     // return api.call("lime-utils", "get_node_status", {});
 };
 
-export const getInternetStatus = () => mock_get_internet_status;
-// api.call("lime-metrics", "get_internet_status", {});
+export const getInternetStatus = async (): Promise<IGetInternetStatus> => {
+    return mock_get_internet_status;
+    // return await api.call("lime-utils", "get_internet_status", {});
+};
 
 const mock_get_internet_status = {
     DNS: {
@@ -111,9 +168,3 @@ const mock_node_status = {
         signal: -14,
     },
 };
-
-const last_internet_path = [
-    { ip: "10.219.105.28", hostname: "nodo11s" },
-    { ip: "10.1.61.129", hostname: "" },
-    { ip: "10.75.0.1", hostname: "" },
-];
