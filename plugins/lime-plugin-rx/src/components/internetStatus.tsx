dc3fe22e36555fbe80ddd14ea72424596dde1410
selankon <selankon@selankon.xyz>
2023-02-27 09:31:14 +0100

    chore(rxPage): improve loading and buttons

--- plugins/lime-plugin-rx/src/components/internetStatus.tsx
+++ plugins/lime-plugin-rx/src/components/internetStatus.tsx
@@ -1,5 +1,6 @@
 import { Trans } from "@lingui/macro";
 
+import { Circle } from "components/icons/circle";
 import { CirclecheckIcon } from "components/icons/circlecheckIcon";
 import { XmarkIcon } from "components/icons/xmarkIcon";
 
@@ -8,6 +9,7 @@ import { IGetInternetStatus } from "plugins/lime-plugin-rx/src/rxApi";
 export const InternetStatus = ({ data }: { data: IGetInternetStatus }) => {
     const checkIconClass = "h-10 w-10 fill-primary-dark ";
     const xmarkIconClass = "h-10 w-10 fill-danger ";
+    const loadiIconClass = "h-10 w-10 stroke-disabled";
 
     return (
         <div class="w-full flex items-center flex-row p-4 gap-6">
@@ -25,7 +27,9 @@ export const InternetStatus = ({ data }: { data: IGetInternetStatus }) => {
                             class="flex justify-center items-center mr-4 mb-4 flex-1 gap-3 mt-4"
                             key={key}
                         >
-                            {value.working ? (
+                            {value.working === null ? (
+                                <Circle className={loadiIconClass} />
+                            ) : value.working ? (
                                 <CirclecheckIcon className={checkIconClass} />
                             ) : (
                                 <XmarkIcon className={xmarkIconClass} />
