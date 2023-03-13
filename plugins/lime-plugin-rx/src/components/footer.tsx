568fd2ceb144bb499d28fb1e46aa756f11b35245
selankon <selankon@selankon.xyz>
2023-03-13 14:18:15 +0100

    chore(rxPage): add footer

---
+++ plugins/lime-plugin-rx/src/components/footer.tsx
@@ -0,0 +1,38 @@
+import { Trans } from "@lingui/macro";
+
+export const Footer = () => {
+    const imgClass = "h-16"
+    return (
+        <div className={"w-full flex justify-around mt-20 content-center items-center"}>
+            <div>
+                <img src={"assets/icons/AlterMundiLogo.svg"} className={imgClass} />
+            </div>
+            <div className={"flex flex-col text-center text-xl"}>
+                <div className={"italic font-normal text-2xl"}>
+                    <Trans>Need support?</Trans>
+                </div>
+                <div>
+                    <Trans>Join</Trans>{" "}
+                    <a
+                        className={"text-[#0198FE] hover:text-[#F39100]"}
+                        href={"https://foro.librerouter.org"}
+                    >
+                        <Trans>foro.librerouter.org</Trans>
+                    </a>
+                </div>
+                <div>
+                    <Trans>Visit</Trans>{" "}
+                    <a
+                        className={"text-[#F39100] hover:text-[#0198FE]"}
+                        href={"https://docs.altermundi.net"}
+                    >
+                        <Trans>docs.altermundi.net</Trans>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <img src={"assets/icons/LibreRouterLogo.svg"} className={imgClass} />
+            </div>
+        </div>
+    );
+};
