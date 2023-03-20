0d147ebacfbaa0f467ac079aa12c2b2a3b8f219b
selankon <selankon@selankon.xyz>
2023-03-20 14:59:15 +0100

    chore(rxPage): disable mocks

--- plugins/lime-plugin-metrics/src/metricsApi.js
+++ plugins/lime-plugin-metrics/src/metricsApi.js
@@ -1,12 +1,16 @@
 import api from "utils/uhttpd.service";
 
 export const getMetrics = (ip) => {
-    console.log("getting metrics for ", ip);
     return api.call("lime-metrics", "get_metrics", { target: ip });
 };
 
 export const getGateway = () =>
     api.call("lime-metrics", "get_gateway", {}).then((res) => res.gateway);
 
-export const getPath = () =>
+export const getPath = async () =>
     api.call("lime-metrics", "get_path", {}).then((res) => res.path);
+
+export const getLoose = async (ip) =>
+    api
+        .call("lime-metrics", "get_loose", { target: ip })
+        .then((res) => res.loose);
