793ee706440126501309aa708d525df276c2c52c
selankon <selankon@selankon.xyz>
2023-03-20 14:59:15 +0100

    chore(rxPage): disable mocks

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,50 +2,26 @@ import { DEFAULT_COMMUNITY_SETTINGS } from "utils/constants";
 import api from "utils/uhttpd.service";
 
 export function getBatHost(mac, outgoingIface) {
-    return mock_bathost.bathost;
-    // return api
-    //     .call("bat-hosts", "get_bathost", {
-    //         mac,
-    //         outgoing_iface: outgoingIface,
-    //     })
-    //     .then(
-    //         (response) =>
-    //             new Promise((res, rej) => {
-    //                 if (response.status === "ok") {
-    //                     res(response.bathost);
-    //                 } else {
-    //                     rej(response.message);
-    //                 }
-    //             })
-    //     );
+    return api
+        .call("bat-hosts", "get_bathost", {
+            mac,
+            outgoing_iface: outgoingIface,
+        })
+        .then(
+            (response) =>
+                new Promise((res, rej) => {
+                    if (response.status === "ok") {
+                        res(response.bathost);
+                    } else {
+                        rej(response.message);
+                    }
+                })
+        );
 }
-
-const mock_bathost = {
-    bathost: {
-        iface: "br-lan",
-        hostname: "lapastoramesh-c",
-    },
-};
 
 export function getBoardData() {
-    return mock_board_data;
-    // return api.call("system", "board", {});
+    return api.call("system", "board", {});
 }
-
-const mock_board_data = {
-    kernel: "4.14.241",
-    hostname: "lapastoramesh-c",
-    system: "Atheros AR9344 rev 2",
-    model: "TP-Link TL-WDR3600 v1",
-    board_name: "tplink,tl-wdr3600-v1",
-    release: {
-        distribution: "%LIME_ID%",
-        version: "%LIME_RELEASE%",
-        revision: "%LIME_REVISION%",
-        target: "ath79/generic",
-        description: "%LIME_DESCRIPTION%",
-    },
-};
 
 export function getSession() {
     return api
