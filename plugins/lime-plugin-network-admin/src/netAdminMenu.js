27a24f14f33259046f1fdee9b9345bc6e4a15384
selankon <selankon@selankon.xyz>
2023-04-18 11:28:38 +0200

    chore(icons): move icons on sepparated files
    In order to reuse them if needed

--- plugins/lime-plugin-network-admin/src/netAdminMenu.js
+++ plugins/lime-plugin-network-admin/src/netAdminMenu.js
@@ -1,10 +1,12 @@
+import { Trans } from "@lingui/macro";
 
-
-import { Trans } from '@lingui/macro';
+import { LockIcon } from "components/icons/teenny/lock";
 
 export const NetAdminMenu = () => (
-	<span>
-		<svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M4.5 6.5v-3a3 3 0 016 0v3m-8 0h10a1 1 0 011 1v6a1 1 0 01-1 1h-10a1 1 0 01-1-1v-6a1 1 0 011-1z" stroke="currentColor" /></svg>
-		<a href='#/netadmin'><Trans>Shared Password</Trans></a>
-	</span>
+    <span>
+        <LockIcon />
+        <a href="#/netadmin">
+            <Trans>Shared Password</Trans>
+        </a>
+    </span>
 );
---
+++ src/components/icons/teenny/lock.tsx
@@ -0,0 +1,14 @@
+export const LockIcon = () => (
+    <svg
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        width="15"
+        height="15"
+    >
+        <path
+            d="M4.5 6.5v-3a3 3 0 016 0v3m-8 0h10a1 1 0 011 1v6a1 1 0 01-1 1h-10a1 1 0 01-1-1v-6a1 1 0 011-1z"
+            stroke="currentColor"
+        />
+    </svg>
+);
