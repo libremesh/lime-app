a55b7701d0b3e588456a8f94fa8b21083fba973b
selankon <selankon@selankon.xyz>
2023-05-03 08:43:56 +0200

    chore(typescript): add missing file
    At some point it was deleteb by error

---
+++ plugins/lime-plugin-node-admin/src/components/config/config.js
@@ -0,0 +1,33 @@
+import { ListItem } from "components/list";
+import Loading from "components/loading";
+
+import style from "./config.style.less";
+
+export const Config = ({
+    title,
+    subtitle,
+    value,
+    onClick,
+    isLoading,
+    ...props
+}) => {
+    return (
+        <ListItem onClick={onClick} {...props}>
+            <div class="d-flex flex-grow-1">
+                <div>
+                    <div class={style.title}> {title} </div>
+                    {isLoading && <Loading />}
+                    {!isLoading && (
+                        <div>
+                            <div class={style.subtitle}> {subtitle} </div>
+                            <div class={style.value}> {value} </div>
+                        </div>
+                    )}
+                </div>
+                <div class={style.arrowWrapper}>
+                    <div class={style.arrow}>›</div>
+                </div>
+            </div>
+        </ListItem>
+    );
+};
