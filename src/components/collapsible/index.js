9e498b5d93b5f393f1beed02870f59ec9d5d4f32
selankon <selankon@selankon.xyz>
2023-05-24 15:26:14 +0200

    chore(src): refactor collapsible component style

--- src/components/collapsible/index.js
+++ src/components/collapsible/index.js
@@ -1,20 +1,22 @@
 import { useToggle } from "react-use";
 
-import style from "./style.less";
+import { ChevronDown, ChevronUp } from "components/icons/teenny/chevrons";
 
 export const Collapsible = ({ title, children, initCollapsed }) => {
     const [collapsed, toggleCollapsed] = useToggle(initCollapsed);
+
     return (
-        <div
-            className={`${style.collapsible} d-flex
-            flex-column cursor-pointer`}
-            onClick={toggleCollapsed}
-        >
-            <div className="d-flex">
-                <div className="flex-grow-1">{title}</div>
-                <div>{collapsed ? "ᐯ" : "ᐱ"}</div>
+        <div className={`flex bg-gray-200 rounded-md flex-col`}>
+            <div
+                className="flex flex-row  items-center justify-between px-6 py-5 cursor-pointer"
+                onClick={toggleCollapsed}
+            >
+                <div className="font-semibold">{title}</div>
+                <div className={""}>
+                    {collapsed ? <ChevronDown /> : <ChevronUp />}
+                </div>
             </div>
-            {!collapsed && <div className="flex-grow-1 mt-1">{children}</div>}
+            {!collapsed && <div>{children}</div>}
         </div>
     );
 };
