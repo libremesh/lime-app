60efaf06312228e01041970eb4dabc7d524b8cbe
selankon <selankon@selankon.xyz>
2024-01-15 16:15:44 +0100

    chore(meshupgrade): implement no new version available

--- plugins/lime-plugin-mesh-wide-upgrade/src/containers/MeshWideUpgradeStatus.tsx
+++ plugins/lime-plugin-mesh-wide-upgrade/src/containers/MeshWideUpgradeStatus.tsx
@@ -1,15 +1,14 @@
 import { useNewVersion } from "plugins/lime-plugin-firmware/src/firmwareQueries";
-import { NewVersionAvailable } from "plugins/lime-plugin-mesh-wide-upgrade/src/components/NewVersionAvailable";
-
-import { useBoardData } from "utils/queries";
+import { NewVersionAvailable } from "plugins/lime-plugin-mesh-wide-upgrade/src/components/upgradeState/NewVersionAvailable";
+import { NoNewVersionAvailable } from "plugins/lime-plugin-mesh-wide-upgrade/src/components/upgradeState/NoNewVersion";
 
 export const MeshWideUpgradeStatus = () => {
-    const { data: boardData } = useBoardData();
     const { data: newVersion } = useNewVersion();
-
+    const isNewVersion = newVersion && newVersion.version;
     return (
         <div className="container container-center">
-            {newVersion && <NewVersionAvailable />}
+            {isNewVersion && <NewVersionAvailable />}
+            {!isNewVersion && <NoNewVersionAvailable />}
         </div>
     );
 };
