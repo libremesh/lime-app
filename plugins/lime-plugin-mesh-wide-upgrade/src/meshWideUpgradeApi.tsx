52db76ca89100cf81e52812488f1ed95c9282382
selankon <selankon@selankon.xyz>
2024-01-25 10:12:11 +0100

    chore(meshupgrade): implement _meshUpgradeApiCall

--- plugins/lime-plugin-mesh-wide-upgrade/src/meshWideUpgradeApi.tsx
+++ plugins/lime-plugin-mesh-wide-upgrade/src/meshWideUpgradeApi.tsx
@@ -1,4 +1,19 @@
+import { MeshWideRPCReturnTypes } from "plugins/lime-plugin-mesh-wide-upgrade/src/meshWideUpgradeTypes";
+import { EupgradeStatus } from "plugins/lime-plugin-mesh-wide-upgrade/src/utils/eupgrade";
+
 import api from "utils/uhttpd.service";
+
+const _meshUpgradeApiCall = async (method: string) => {
+    const res = (await api.call(
+        "lime-mesh-upgrade",
+        method
+    )) as MeshWideRPCReturnTypes;
+    if (res.error) {
+        // todo(kon): handle errors with error code or whatever
+        throw new Error(res.error);
+    }
+    return res.code;
+};
 
 export const getMeshWideUpgradeInfo = async () => {
     return api.call("shared-state", "getFromSharedState", {
@@ -6,10 +21,16 @@ export const getMeshWideUpgradeInfo = async () => {
     });
 };
 
-export const setUpLocalRepository = async () => {
-    return api.call("lime-mesh-upgrade", "set_up_local_repository");
+export const setBecomeMainNode = async () => {
+    return await _meshUpgradeApiCall("become_main_node");
 };
 
-export const setBecomeMainNode = async () => {
-    return api.call("lime-mesh-upgrade", "become_main_node");
+export const getMainNodeStatus = async () => {
+    return (await _meshUpgradeApiCall(
+        "get_main_node_status"
+    )) as EupgradeStatus;
+};
+
+export const setStartFirmwareUpgradeTransaction = async () => {
+    return await _meshUpgradeApiCall("start_firmware_upgrade_transaction");
 };
