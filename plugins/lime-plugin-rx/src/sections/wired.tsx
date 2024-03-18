d63fb57e26d814645c0c5348513159163e2ae596
selankon <selankon@selankon.xyz>
2024-03-18 15:58:53 +0100

    chore(rxPage): show no wired connections found error message

--- plugins/lime-plugin-rx/src/sections/wired.tsx
+++ plugins/lime-plugin-rx/src/sections/wired.tsx
@@ -6,11 +6,11 @@ import {
     SectionTitle,
 } from "plugins/lime-plugin-rx/src/components/components";
 import { PortsIcon } from "plugins/lime-plugin-rx/src/icons/portsIcon";
-import { StatusResponse } from "plugins/lime-plugin-rx/src/rxApi";
+import { SwitchStatus } from "plugins/lime-plugin-rx/src/rxApi";
 import { useNodeStatus } from "plugins/lime-plugin-rx/src/rxQueries";
 
-const Ports = ({ status }: { status: StatusResponse }) => {
-    const ports = status.switch_status.reduce((acc, obj) => {
+const Ports = ({ switches }: { switches: SwitchStatus[] }) => {
+    const ports = switches.reduce((acc, obj) => {
         const { role } = obj;
         if (!acc[role]) {
             acc[role] = [];
@@ -21,7 +21,6 @@ const Ports = ({ status }: { status: StatusResponse }) => {
 
     return (
         <div className={"flex flex-row px-10"} data-testid="ports-container">
-            {" "}
             {Object.keys(ports).map((role) => {
                 if (
                     role.toLowerCase() === "wan" ||
@@ -58,6 +57,8 @@ const Ports = ({ status }: { status: StatusResponse }) => {
 export const Wired = () => {
     const { data: status, isLoading } = useNodeStatus();
 
+    const switches = status?.switch_status;
+
     return (
         <Section>
             <SectionTitle icon={<PortsIcon className={IconsClassName} />}>
@@ -66,8 +67,12 @@ export const Wired = () => {
             <div className={"mt-4"}>
                 {isLoading ? (
                     <span>Loading...</span>
+                ) : switches.length ? (
+                    <Ports switches={status.switch_status} />
                 ) : (
-                    <Ports status={status} />
+                    <div className={"flex-1 flex justify-center"}>
+                        No wired connections found
+                    </div>
                 )}
             </div>
         </Section>
