386ecf5463be8233421aa1a0a85aa2bd23e67d8d
selankon <selankon@selankon.xyz>
2024-08-28 16:59:34 +0200

    chore(meshwideupgrade): add info when no nodes

--- plugins/lime-plugin-mesh-wide-upgrade/src/containers/nodesList.tsx
+++ plugins/lime-plugin-mesh-wide-upgrade/src/containers/nodesList.tsx
@@ -1,13 +1,33 @@
+import { Trans } from "@lingui/macro";
+
 import Loading from "components/loading";
 
 import NodeUpgradeInfoItem from "plugins/lime-plugin-mesh-wide-upgrade/src/components/nodeUpgradeInfo";
+import { UpgradeState } from "plugins/lime-plugin-mesh-wide-upgrade/src/components/upgradeState/UpgradeState";
 import { useMeshUpgrade } from "plugins/lime-plugin-mesh-wide-upgrade/src/hooks/meshWideUpgradeProvider";
+import { CenterFlex } from "plugins/lime-plugin-mesh-wide-upgrade/src/utils/divs";
 
 export const NodesList = () => {
     const { data, isLoading } = useMeshUpgrade();
 
-    if (isLoading || data === undefined) {
+    if (isLoading) {
         return <Loading />;
+    }
+
+    if (!data || (data && Object.keys(data).length === 0)) {
+        return (
+            <CenterFlex>
+                <UpgradeState
+                    icon={null}
+                    title={
+                        <Trans>
+                            No nodes present on the <br />
+                            mesh wide upgrade state yet!
+                        </Trans>
+                    }
+                />
+            </CenterFlex>
+        );
     }
 
     return (
