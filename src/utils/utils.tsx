5d12601cd6f92d234d48fdf7d8283ecc0596782c
selankon <selankon@selankon.xyz>
2024-09-04 17:15:30 +0200

    chore(mesh-wide): chore

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -3,7 +3,6 @@ export function isEmpty(obj: object | null | undefined): boolean {
     if (obj == null || typeof obj !== "object") {
         return true;
     }
-
     // Return true if the object has no own properties
     return Object.keys(obj).length === 0;
 }
